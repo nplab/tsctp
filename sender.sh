000506ce690c01be84690fbea9cdf0e66064225c
Felix Weinrank <info@weinrank.net>
2016-01-06 14:04:59 +0100

    Add loop functionality

--- sender.sh
+++ sender.sh
@@ -6,19 +6,24 @@ REMOTE_ADDR="fd68:9c38:be7e::14"
 DURATION="60"
 BUFFER="262144"
 APPENDIX=""
+LOOPS="1"
 
 MSGLENGTH=0
+LOOP=1
 
-while [ $MSGLENGTH -lt 1600 ]; do
-    if [ $MSGLENGTH -lt 32 ]; then
-        MSGLENGTH=$((MSGLENGTH + 1))
-    elif [ $MSGLENGTH -lt 128 ]; then
-        MSGLENGTH=$((MSGLENGTH + 4))
-    else
-        MSGLENGTH=$((MSGLENGTH + 16))
-    fi
+while [ $LOOP -le $LOOPS]; do
+    while [ $MSGLENGTH -lt 1600 ]; do
+        if [ $MSGLENGTH -lt 32 ]; then
+            MSGLENGTH=$((MSGLENGTH + 1))
+        elif [ $MSGLENGTH -lt 128 ]; then
+            MSGLENGTH=$((MSGLENGTH + 4))
+        else
+            MSGLENGTH=$((MSGLENGTH + 16))
+        fi
 
-    echo "LENGTH: $MSGLENGTH"
-    ./tsctp -T $DURATION -L $LOCAL_ADDR -l $MSGLENGTH -R $BUFFER -S $BUFFER $APPENDIX $REMOTE_ADDR
-    sleep 5
+        echo "LENGTH: $MSGLENGTH"
+        ./tsctp -T $DURATION -L $LOCAL_ADDR -l $MSGLENGTH -R $BUFFER -S $BUFFER $APPENDIX $REMOTE_ADDR
+        sleep 5
+    done
+    $LOOP=$((LOOP + 1))
 done
